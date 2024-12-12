54d55d9bf67b3077c6ffe8ec9e9b7ff4fac17e0f
fush0528 <167970556+fush0528@users.noreply.github.com>
2024-12-12 15:55:07 +0800

    update users model

--- app/models.py
+++ app/models.py
@@ -8,3 +8,10 @@ class Todo(Base):
         description=Column(String,nullable=False)
         completed=Column(Boolean,default=False)
         due_date = Column(Date, nullable=True)
+
+class Users(Base):
+        __tablename__="Users"
+        id=Column(Integer, primary_key=True, index=True)
+        username=Column(String,nullable=False)
+        password=Column(String,nullable=False)
+        email = Column(String, unique=True, nullable=False)
